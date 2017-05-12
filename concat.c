638fca8ff88ea2da02500fe5de850797fda2aff2
Pedro Sena <peufisbach@gmail.com>
2017-05-11 22:28:12 -0300

    concat.c added
    Just a small little example of using argc and argv

---
+++ concat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+  char *saida;
+  int i, tam = 1, count = 0, j;
+  if(argc >= 3){
+    for(i = 1; i < argc; i++) tam += strlen(argv[i]);
+    saida=(char*)malloc(tam * sizeof(char));
+    for(i = 1; i < argc; i++){
+      for(j = 0; argv[i][j] != '\0'; j++){
+        saida[count] = argv[i][j];
+        count++;
+      }
+    }
+    saida[count]='\0';
+    printf("%s\n",saida);
+  } else if(argc == 2){
+    if(!strcmp(argv[1],"help")) printf("Este programa concatena duas ou\n mais strings fornecidas como argumentos.\n");
+    else printf("%s\n",argv[1]);
+  }
+  return 0;
+}
